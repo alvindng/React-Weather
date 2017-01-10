510de74a720b079e926f3aeffe26fc747fd558ec
Alvin Ng <alvindng@gmail.com>
2017-01-10 14:40:41 -0600

    add styles to weather page

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var WeatherMessage = ({temp,location}) => {
   return (
-    <h3>It is {temp} in {location}</h3>
+    <h3 className='text-center'>It is {temp} in {location}</h3>
   );
 };
 //you can included destructured variables directly in the arguments of a function. it is the same as calling props.temp and props.location

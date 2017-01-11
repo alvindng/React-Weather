527ca12777703f2cbde098fb8a952bc7ebc800ea
Alvin Ng <alvindng@gmail.com>
2017-01-10 21:12:29 -0600

    add custom styles support

--- app/app.jsx
+++ app/app.jsx
@@ -20,6 +20,9 @@ $(document).foundation();
 
 //when you have a parent component thats handling an event from the child you usually call the parent 'handle' followed by the name you chose and then you call it in the child with 'on' followed by the name you chose. ie onNewData (child) vs handleNewData (parent)
 
+//App css
+require('style!css!applicationStyles') //two loaders
+
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
